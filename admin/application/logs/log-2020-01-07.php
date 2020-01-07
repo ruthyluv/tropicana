e34abf1efee8dad7bf6b88c32da253999a4de48e
Kobby Opoku <breezeexp1@gmail.com>
2020-01-07 17:52:19 +0000

    bug fixed on database connection

---
+++ admin/application/logs/log-2020-01-07.php
@@ -0,0 +1,26 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-01-07 17:46:43 --> Severity: Warning  --> mysqli_connect(): (HY000/1049): Unknown database 'hotel' C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_driver.php 77
+ERROR - 2020-01-07 17:46:43 --> Unable to connect to the database
+ERROR - 2020-01-07 17:46:44 --> Severity: Warning  --> mysqli_connect(): (HY000/1049): Unknown database 'hotel' C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_driver.php 77
+ERROR - 2020-01-07 17:46:44 --> Unable to connect to the database
+ERROR - 2020-01-07 17:46:44 --> Severity: Notice  --> Undefined variable: error C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 11
+ERROR - 2020-01-07 18:06:05 --> Severity: Warning  --> mysqli_connect(): (HY000/1049): Unknown database 'hotel' C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_driver.php 77
+ERROR - 2020-01-07 18:06:05 --> Unable to connect to the database
+ERROR - 2020-01-07 18:06:05 --> Severity: Warning  --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_driver.php 316
+ERROR - 2020-01-07 18:06:05 --> Severity: Warning  --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_driver.php 316
+ERROR - 2020-01-07 18:06:05 --> Severity: Warning  --> mysqli_connect(): (HY000/1049): Unknown database 'hotel' C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_driver.php 77
+ERROR - 2020-01-07 18:06:05 --> Unable to connect to the database
+ERROR - 2020-01-07 18:06:05 --> Severity: Warning  --> mysqli_query() expects parameter 1 to be mysqli, boolean given C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_driver.php 180
+ERROR - 2020-01-07 18:06:05 --> Severity: Warning  --> mysqli_num_rows() expects parameter 1 to be mysqli_result, null given C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_result.php 38
+ERROR - 2020-01-07 18:06:05 --> Severity: Warning  --> mysqli_num_rows() expects parameter 1 to be mysqli_result, null given C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_result.php 38
+ERROR - 2020-01-07 18:46:42 --> Severity: Notice  --> Undefined variable: error C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 11
+ERROR - 2020-01-07 18:47:52 --> Severity: Notice  --> Undefined index: roomservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 16
+ERROR - 2020-01-07 18:47:52 --> Severity: Notice  --> Undefined index: restaurant C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 19
+ERROR - 2020-01-07 18:47:52 --> Severity: Notice  --> Undefined index: sport C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 22
+ERROR - 2020-01-07 18:47:52 --> Severity: Notice  --> Undefined index: medicalservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 25
+ERROR - 2020-01-07 18:47:52 --> Severity: Notice  --> Undefined index: laundry C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-07 18:47:52 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
+ERROR - 2020-01-07 18:47:52 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-07 18:47:52 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-07 18:48:38 --> Severity: Warning  --> Creating default object from empty value C:\xampp\htdocs\tropicana\admin\application\models\room_m.php 35
