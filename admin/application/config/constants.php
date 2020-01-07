ec970963a80417de423e655b5fb62ce614c5cbcc
Kobby Opoku <breezeexp1@gmail.com>
2020-01-07 19:05:34 +0000

    bug fix for room types

--- admin/application/config/constants.php
+++ admin/application/config/constants.php
@@ -1,8 +1,8 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 
 
-define('HOTEL_NAME', "DedeBan Hotel");
+define('HOTEL_NAME', "Tropicana Hotel");
 
 
 /*
@@ -17,7 +17,7 @@ define('HOTEL_NAME', "DedeBan Hotel");
 | user, PHP under CGI with Apache suEXEC, etc.).  Octal values should
 | always be used to set the mode correctly.
 |
-*/
+ */
 define('FILE_READ_MODE', 0644);
 define('FILE_WRITE_MODE', 0666);
 define('DIR_READ_MODE', 0755);
@@ -30,16 +30,16 @@ define('DIR_WRITE_MODE', 0777);
 |
 | These modes are used when working with fopen()/popen()
 |
-*/
+ */
 
-define('FOPEN_READ',							'rb');
-define('FOPEN_READ_WRITE',						'r+b');
-define('FOPEN_WRITE_CREATE_DESTRUCTIVE',		'wb'); // truncates existing file data, use with care
-define('FOPEN_READ_WRITE_CREATE_DESTRUCTIVE',	'w+b'); // truncates existing file data, use with care
-define('FOPEN_WRITE_CREATE',					'ab');
-define('FOPEN_READ_WRITE_CREATE',				'a+b');
-define('FOPEN_WRITE_CREATE_STRICT',				'xb');
-define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
+define('FOPEN_READ', 'rb');
+define('FOPEN_READ_WRITE', 'r+b');
+define('FOPEN_WRITE_CREATE_DESTRUCTIVE', 'wb'); // truncates existing file data, use with care
+define('FOPEN_READ_WRITE_CREATE_DESTRUCTIVE', 'w+b'); // truncates existing file data, use with care
+define('FOPEN_WRITE_CREATE', 'ab');
+define('FOPEN_READ_WRITE_CREATE', 'a+b');
+define('FOPEN_WRITE_CREATE_STRICT', 'xb');
+define('FOPEN_READ_WRITE_CREATE_STRICT', 'x+b');
 
 
 /* End of file constants.php */
--- admin/application/logs/log-2020-01-07.php
+++ admin/application/logs/log-2020-01-07.php
@@ -24,3 +24,38 @@ ERROR - 2020-01-07 18:47:52 --> Severity: Notice  --> Undefined index: massage C
 ERROR - 2020-01-07 18:47:52 --> 404 Page Not Found --> welcome/img
 ERROR - 2020-01-07 18:47:52 --> 404 Page Not Found --> welcome/img
 ERROR - 2020-01-07 18:48:38 --> Severity: Warning  --> Creating default object from empty value C:\xampp\htdocs\tropicana\admin\application\models\room_m.php 35
+ERROR - 2020-01-07 19:05:22 --> Severity: Notice  --> Undefined variable: error C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 11
+ERROR - 2020-01-07 19:10:16 --> Severity: Notice  --> Undefined variable: error C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 11
+ERROR - 2020-01-07 19:10:27 --> Severity: Notice  --> Undefined index: roomservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 16
+ERROR - 2020-01-07 19:10:27 --> Severity: Notice  --> Undefined index: restaurant C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 19
+ERROR - 2020-01-07 19:10:27 --> Severity: Notice  --> Undefined index: sport C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 22
+ERROR - 2020-01-07 19:10:27 --> Severity: Notice  --> Undefined index: medicalservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 25
+ERROR - 2020-01-07 19:10:27 --> Severity: Notice  --> Undefined index: laundry C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-07 19:10:27 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
+ERROR - 2020-01-07 19:10:27 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-07 19:10:27 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-07 19:10:33 --> Severity: Warning  --> Creating default object from empty value C:\xampp\htdocs\tropicana\admin\application\models\room_m.php 35
+ERROR - 2020-01-07 19:11:47 --> Severity: Notice  --> Undefined index: roomservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 16
+ERROR - 2020-01-07 19:11:47 --> Severity: Notice  --> Undefined index: restaurant C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 19
+ERROR - 2020-01-07 19:11:47 --> Severity: Notice  --> Undefined index: sport C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 22
+ERROR - 2020-01-07 19:11:47 --> Severity: Notice  --> Undefined index: medicalservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 25
+ERROR - 2020-01-07 19:11:47 --> Severity: Notice  --> Undefined index: laundry C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-07 19:11:47 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
+ERROR - 2020-01-07 19:13:36 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 5
+ERROR - 2020-01-07 19:13:36 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 13
+ERROR - 2020-01-07 19:13:36 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 18
+ERROR - 2020-01-07 19:13:36 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 24
+ERROR - 2020-01-07 19:13:36 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-07 19:16:29 --> Severity: Warning  --> Creating default object from empty value C:\xampp\htdocs\tropicana\admin\application\models\room_m.php 35
+ERROR - 2020-01-07 19:27:15 --> Severity: Notice  --> Undefined index: roomservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 16
+ERROR - 2020-01-07 19:27:15 --> Severity: Notice  --> Undefined index: restaurant C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 19
+ERROR - 2020-01-07 19:27:15 --> Severity: Notice  --> Undefined index: sport C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 22
+ERROR - 2020-01-07 19:27:15 --> Severity: Notice  --> Undefined index: medicalservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 25
+ERROR - 2020-01-07 19:27:15 --> Severity: Notice  --> Undefined index: laundry C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-07 19:27:15 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
+ERROR - 2020-01-07 19:52:38 --> Severity: Notice  --> Undefined index: roomservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 16
+ERROR - 2020-01-07 19:52:38 --> Severity: Notice  --> Undefined index: restaurant C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 19
+ERROR - 2020-01-07 19:52:38 --> Severity: Notice  --> Undefined index: sport C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 22
+ERROR - 2020-01-07 19:52:38 --> Severity: Notice  --> Undefined index: medicalservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 25
+ERROR - 2020-01-07 19:52:38 --> Severity: Notice  --> Undefined index: laundry C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-07 19:52:38 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
