f98c95676f18709c6914f71d73278cc9e0addb36
Kobby Opoku <breezeexp1@gmail.com>
2020-01-08 11:13:22 +0000

    Logs changed

---
+++ admin/application/logs/log-2020-01-08.php
@@ -0,0 +1,19 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-01-08 11:52:20 --> Severity: Notice  --> Undefined variable: error C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 11
+ERROR - 2020-01-08 11:52:33 --> Severity: Notice  --> Undefined index: roomservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 16
+ERROR - 2020-01-08 11:52:33 --> Severity: Notice  --> Undefined index: restaurant C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 19
+ERROR - 2020-01-08 11:52:33 --> Severity: Notice  --> Undefined index: sport C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 22
+ERROR - 2020-01-08 11:52:33 --> Severity: Notice  --> Undefined index: medicalservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 25
+ERROR - 2020-01-08 11:52:33 --> Severity: Notice  --> Undefined index: laundry C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-08 11:52:33 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
+ERROR - 2020-01-08 11:52:34 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-08 11:52:34 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-08 11:52:43 --> Severity: Notice  --> Undefined index: roomservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 16
+ERROR - 2020-01-08 11:52:43 --> Severity: Notice  --> Undefined index: restaurant C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 19
+ERROR - 2020-01-08 11:52:43 --> Severity: Notice  --> Undefined index: sport C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 22
+ERROR - 2020-01-08 11:52:43 --> Severity: Notice  --> Undefined index: medicalservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 25
+ERROR - 2020-01-08 11:52:43 --> Severity: Notice  --> Undefined index: laundry C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-08 11:52:43 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
+ERROR - 2020-01-08 11:52:44 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-08 11:52:44 --> 404 Page Not Found --> welcome/img
