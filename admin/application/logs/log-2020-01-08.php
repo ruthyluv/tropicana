fc2b63b52a2ad589a27026522a1f9b4c0953faf8
ruthyluv <ruthowusuansah0@gmail.com>
2020-01-08 17:46:28 +0000

    modal for client details added

--- admin/application/logs/log-2020-01-08.php
+++ admin/application/logs/log-2020-01-08.php
@@ -17,3 +17,60 @@ ERROR - 2020-01-08 11:52:43 --> Severity: Notice  --> Undefined index: laundry C
 ERROR - 2020-01-08 11:52:43 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
 ERROR - 2020-01-08 11:52:44 --> 404 Page Not Found --> welcome/img
 ERROR - 2020-01-08 11:52:44 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-08 14:59:04 --> Severity: Warning  --> mysqli_connect(): (HY000/1049): Unknown database 'hotel' C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_driver.php 77
+ERROR - 2020-01-08 14:59:04 --> Unable to connect to the database
+ERROR - 2020-01-08 14:59:04 --> Severity: Warning  --> mysqli_connect(): (HY000/1049): Unknown database 'hotel' C:\xampp\htdocs\tropicana\admin\system\database\drivers\mysqli\mysqli_driver.php 77
+ERROR - 2020-01-08 14:59:04 --> Unable to connect to the database
+ERROR - 2020-01-08 14:59:05 --> Severity: Notice  --> Undefined variable: error C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 11
+ERROR - 2020-01-08 15:03:55 --> Severity: Notice  --> Undefined index: roomservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 16
+ERROR - 2020-01-08 15:03:55 --> Severity: Notice  --> Undefined index: restaurant C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 19
+ERROR - 2020-01-08 15:03:55 --> Severity: Notice  --> Undefined index: sport C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 22
+ERROR - 2020-01-08 15:03:55 --> Severity: Notice  --> Undefined index: medicalservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 25
+ERROR - 2020-01-08 15:03:55 --> Severity: Notice  --> Undefined index: laundry C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-08 15:03:55 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
+ERROR - 2020-01-08 15:03:55 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-08 15:03:55 --> 404 Page Not Found --> welcome/img
+ERROR - 2020-01-08 15:39:09 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 5
+ERROR - 2020-01-08 15:39:10 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 13
+ERROR - 2020-01-08 15:39:10 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 18
+ERROR - 2020-01-08 15:39:10 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 24
+ERROR - 2020-01-08 15:39:10 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-08 15:39:53 --> Severity: Warning  --> Missing argument 1 for Room_type::edit() C:\xampp\htdocs\tropicana\admin\application\controllers\room_type.php 61
+ERROR - 2020-01-08 15:39:53 --> Severity: Notice  --> Undefined variable: room_type C:\xampp\htdocs\tropicana\admin\application\controllers\room_type.php 77
+ERROR - 2020-01-08 15:39:53 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 5
+ERROR - 2020-01-08 15:39:53 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 13
+ERROR - 2020-01-08 15:39:53 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 18
+ERROR - 2020-01-08 15:39:53 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 24
+ERROR - 2020-01-08 15:39:53 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-08 15:40:01 --> Severity: Warning  --> Missing argument 1 for Room_type::edit() C:\xampp\htdocs\tropicana\admin\application\controllers\room_type.php 61
+ERROR - 2020-01-08 15:40:01 --> Severity: Notice  --> Undefined variable: room_type C:\xampp\htdocs\tropicana\admin\application\controllers\room_type.php 77
+ERROR - 2020-01-08 15:40:01 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 5
+ERROR - 2020-01-08 15:40:01 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 13
+ERROR - 2020-01-08 15:40:01 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 18
+ERROR - 2020-01-08 15:40:01 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 24
+ERROR - 2020-01-08 15:40:01 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-08 15:58:03 --> Severity: Warning  --> Missing argument 1 for Room_type::edit() C:\xampp\htdocs\tropicana\admin\application\controllers\room_type.php 61
+ERROR - 2020-01-08 15:58:04 --> Severity: Notice  --> Undefined variable: room_type C:\xampp\htdocs\tropicana\admin\application\controllers\room_type.php 77
+ERROR - 2020-01-08 15:58:04 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 5
+ERROR - 2020-01-08 15:58:04 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 13
+ERROR - 2020-01-08 15:58:04 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 18
+ERROR - 2020-01-08 15:58:04 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 24
+ERROR - 2020-01-08 15:58:04 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-08 15:58:08 --> Severity: Notice  --> Undefined index: roomservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 16
+ERROR - 2020-01-08 15:58:08 --> Severity: Notice  --> Undefined index: restaurant C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 19
+ERROR - 2020-01-08 15:58:08 --> Severity: Notice  --> Undefined index: sport C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 22
+ERROR - 2020-01-08 15:58:08 --> Severity: Notice  --> Undefined index: medicalservice C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 25
+ERROR - 2020-01-08 15:58:08 --> Severity: Notice  --> Undefined index: laundry C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-08 15:58:08 --> Severity: Notice  --> Undefined index: massage C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 32
+ERROR - 2020-01-08 15:58:40 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 5
+ERROR - 2020-01-08 15:58:40 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 13
+ERROR - 2020-01-08 15:58:40 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 18
+ERROR - 2020-01-08 15:58:40 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 24
+ERROR - 2020-01-08 15:58:40 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
+ERROR - 2020-01-08 15:58:47 --> Severity: Warning  --> Missing argument 1 for Room_type::edit() C:\xampp\htdocs\tropicana\admin\application\controllers\room_type.php 61
+ERROR - 2020-01-08 15:58:47 --> Severity: Notice  --> Undefined variable: room_type C:\xampp\htdocs\tropicana\admin\application\controllers\room_type.php 77
+ERROR - 2020-01-08 15:58:47 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 5
+ERROR - 2020-01-08 15:58:47 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 13
+ERROR - 2020-01-08 15:58:47 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 18
+ERROR - 2020-01-08 15:58:47 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 24
+ERROR - 2020-01-08 15:58:47 --> Severity: Notice  --> Trying to get property of non-object C:\xampp\htdocs\tropicana\admin\system\core\Loader.php(830) : eval()'d code 29
